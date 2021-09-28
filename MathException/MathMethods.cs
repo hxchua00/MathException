d18d92c7cfc615d6fe1b7efbc1537834dca7ba7a
hxchua00 <hxchua00@gmail.com>
2021-09-28 14:22:20 +0800

    Add project files.

---
+++ MathException/MathMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathException
+{
+    class MathMethods
+    {
+        static double result;
+        public static double Multiply(double x, double y)
+        {
+            try
+            {
+                result = x * y;
+            }
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            catch (ArithmeticException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            try
+            {
+                result = x / y;
+            }
+            catch (ArgumentException)
+            {
+                throw;
+            }
+            catch (ArithmeticException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+    }
+}
---
+++ MathException/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0, y = 0;
+            double result = 0;
+            Console.WriteLine("Enter 2 numbers: ");
+            try
+            {
+                Console.Write("First Number: ");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Second Number: ");
+                y = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalide input! Only numbers are allowed!");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Non-Inegers are not allowed!");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong! Please check through everything!");
+                Console.WriteLine();
+            }
+            result = MathMethods.Multiply(x, y);
+            Console.WriteLine("Multiplication: " + result);
+            result = MathMethods.Divide(x, y);
+            Console.WriteLine("Division: " + result);
+            Console.ReadLine();
+        }
+    }
+}
